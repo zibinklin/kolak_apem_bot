fef0d6c2bdbb11947e547cdf15f2dd2922072f76
M Afif Nur Azizi <afifnurazizi@gmail.com>
2020-12-10 00:15:25 +0800

    send message dan reply text

--- chat_lib/test_bot.py
+++ chat_lib/test_bot.py
@@ -1,15 +1,25 @@
-from telegram.ext import Updater
+from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
+from telegram import files
+import sys
+from telegram import Bot
 import logging
 
-updater = Updater(token='1470794848:AAGLbv_OxiLNU9WR4XzOcW2KXr3KCSx0PgA',use_context=True)
-print(f'value of updater :{updater} ')
-
-dispacher = updater.dispatcher
-print(f'value of dispacher : {dispacher}')
-
-logging_value = logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-print(f'value dari logging : {logging_value}')
-
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.effective_chat.id)
+
+def message(update,context):
+    update.message.reply_text(update.message.text)
+
+def command_handler(update,contex):
+    update.message.reply_text(f'mendeteksi bahwa {update.message.text} adalah command')
+
+def main():
+    updater = Updater(token='1470794848:AAGLbv_OxiLNU9WR4XzOcW2KXr3KCSx0PgA',use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(MessageHandler(Filters.text,message))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+   main()
